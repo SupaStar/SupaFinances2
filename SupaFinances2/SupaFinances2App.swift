628fdd5594f7fcb65ec862dd1c563bd283612299
Obed Noe Martinez Gonzalez <68682799+SupaStar@users.noreply.github.com>
2023-08-16 12:57:01 -0600

    Initial Commit

---
+++ SupaFinances2/SupaFinances2App.swift
@@ -0,0 +1,22 @@
+//
+//  SupaFinances2App
+//  SupaFinances2
+//
+//  Created by Obed Martinez on 16/08/23
+//
+
+
+
+import SwiftUI
+
+@main
+struct SupaFinances2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ SupaFinances2UITests/SupaFinances2UITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  SupaFinances2UITestsLaunchTests
+//  SupaFinances2UITests
+//
+//  Created by Obed Martinez on 16/08/23
+//
+
+
+
+import XCTest
+
+final class SupaFinances2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
